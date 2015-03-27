645be59c50f26d8fd38aa7a0ec2fd04ebe96a367
roy <roy_radio@126.com>
2015-03-27 10:46:30 +0800

    section 3.3

--- chapter03/app.js
+++ chapter03/app.js
@@ -48,6 +48,93 @@
         jump();
 
         window.addEventListener('hashchange', jump, false);
+
+        var editVisualButton = document.getElementById('edit_visual'),
+            visualView = document.getElementById('file_contents_visual'),
+            visualEditor = document.getElementById('file_contents_visual_editor'),
+            visualEditorDoc = visualEditor.contentDocument,
+            editHtmlButton = document.getElementById('edit_html'),
+            htmlView = document.getElementById('file_contents_html'),
+            htmlEditor = document.getElementById('file_contents_html_editor');
+
+        visualEditorDoc.designMode = 'on';
+
+        visualEditorDoc.addEventListener('keyup', markDirty, false);
+        htmlEditor.addEventListener('keyup', markDirty, false);
+
+        var updateVisualEditor = function(content) {
+            visualEditorDoc.open();
+            visualEditorDoc.write(content);
+            visualEditorDoc.close();
+            visualEditorDoc.addEventListener('keyup', markDirty, false);
+        };
+
+        var updateHtmlEditor = function(content) {
+            htmlEditor.value = content;
+        };
+
+        var toggleActiveView = function() {
+            if (htmlView.style.display == 'block') {
+                editVisualButton.className = 'split_left_active';
+                visualView.style.display = 'block';
+                editHtmlButton.className = 'split_right';
+                htmlView.style.display = 'none';
+                updateVisualEditor(htmlEditor.value);
+            } else {
+                editHtmlButton.className = 'split_right active';
+                htmlView.style.display = 'block';
+                editVisualButton.className = 'split_left';
+                visualView.style.display = 'none';
+
+                var x = new XMLSerializer();
+                var content = x.serializeToString(visualEditorDoc);
+                updateHtmlEditor(content);
+            }
+        }
+
+        editVisualButton.addEventListener('click', toggleActiveView, false);
+        editHtmlButton.addEventListener('click', toggleActiveView, false);
+
+        var visualEditorToolbar = document.getElementById('file_contents_visual_toolbar');
+
+        var richTextAction = function(e) {
+            var command,
+                node = (e.target.nodeName === "BUTTON") ? e.target :
+                e.target.parentNode;
+
+            if (node.dataset) {
+                command = node.dataset.command;
+            } else {
+                command = node.getAttribute('data-command');
+            }
+
+            var doPopupCommand = function(command, promptText, promptDefault) {
+                visualEditorDoc.execCommand(command, false, prompt(promptText, promptDefault));
+            }
+
+            if (command === 'createLink') {
+                doPopupCommand(command, 'Enter link URL:', 'http://www.example.com');
+            } else if (command === 'insertImage') {
+                doPopupCommand(command, 'Enter image URL:', 'http://www.example.com/image.png');
+            } else if (command === 'insertMap') {
+                if (navigator.geolocation) {
+                    node.innerHTML = 'Loading';
+                    navigator.geolocation.getCurrentPosition(function(pos) {
+                        var coords = pos.coords.latitude + ', ' + pos.coords.longitude;
+                        var img = 'http://maps.googleapis.com/maps/api/staticmap?markers='
+                            + coords + '&zoom=11&size=200x200&sensor=false';
+                        visualEditorDoc.execCommand('insertImage', false, img);
+                        node.innerHTML = 'Location Map';
+                    });
+                } else {
+                    alert('Geolocation not available', 'No geolocation data');
+                }
+            } else {
+                visualEditorDoc.execCommand(command);
+            }
+        };
+
+        visualEditorToolbar.addEventListener('click', richTextAction, false);
     };
 
     var init = function() {
